f5bcb9dc63590f7c4e286cf94ee8a63b4709bde1
nearlynithin <nearlynithin@gmail.com>
2024-02-20 11:43:20 +0530

    db

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY']="nahi'dwin"
+    app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
 
     app.register_blueprint(views, url_prefix='/')
+
+    from .models import  Citizen, Complaint, Employee
+
+    with app.app_context():
+        db.create_all()
+    
     return app
---
+++ website/models.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from datetime import datetime
+from . import db
+
+class Citizen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    complaints = db.relationship('Complaint', backref='citizen', lazy=True)
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    assigned_complaints = db.relationship('Complaint', backref='employee', lazy=True)
+
+class Complaint(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    complaint_text = db.Column(db.String(500), nullable=False)
+    date_submitted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    is_completed = db.Column(db.Boolean, nullable=False, default=False)
+    citizen_id = db.Column(db.Integer, db.ForeignKey('citizen.id'), nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'))
