61fcb429bd78dec1bf07facc95fc72c3f18b7769
SANTHSIM22 <santhsimviriledsouza123456@gmail.com>
2024-02-21 01:58:58 +0530

    bug bugo bug

--- website/__init__.py
+++ website/__init__.py
@@ -32,6 +32,7 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(id):
-        return Citizen.query.get(int(id))
+        print(1)
+        return Citizen.query.get(id)
 
     return app
