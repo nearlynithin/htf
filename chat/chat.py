c3f1dd121234143170b307e81b9249a08f18e207
Peng-uinni <23c35.rayan@sjec.ac.in>
2024-02-20 22:07:03 +0530

    temp home

--- chat/chat.py
+++ chat/chat.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room, send
 import random
 from string import ascii_uppercase
@@ -23,39 +23,69 @@ def generate_unique_code(length):
     return code
 
 @app.route("/", methods=['POST', 'GET'])
-def chat():
+def home():
     session.clear()
-    room = generate_unique_code(4)
-    rooms[room] = {"members":0, "messages":[]}
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
 
-    session["room"] = room
+        if not name:
+            return render_template("tempface.html", error="Room wa shindeiru")
+        
+        if join != False and not code:
+            return render_template("tempface.html", error="mf enter a goddamn code")
+        
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members":0, "messages":[]}
+        elif code not in rooms:
+            return render_template("tempface.html", error="No room", code=code, name=name)
     
-    return render_template("chat.html", code=room)
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
+    
+    return render_template("tempface.html")
+
+@app.route("/chat")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    
+    return render_template("chat.html", code=room, messages=rooms[room]["messages"])
 
 @sio.on("connect")
 def connect(auth):
     room = session.get("room")
-    if not room:
+    name = session.get("name")
+    if not room or not name:
         return
     if room not in rooms:
         leave_room(room)
         return 
     
     join_room(room)
-    send({"message": "welcome"}, to=room)
+    send({"message": "welcome", "name": name}, to=room)
     rooms[room]["members"] += 1
-    print(f"joined room {room}")
+    print(f"{name} joined room {room}")
 
 @sio.on("disconnect")
 def disconnect():
     room = session.get("room")
+    name = session.get("name")
     leave_room(room)
 
     if room in rooms:
         rooms[room]["members"] -= 1
         if rooms[room]["members"] <= 0:
             del rooms[room]
-    print(f"left the room {room}")
+
+    send({"name": name, "message": "has left"}, to=room)
+    print(f"{name} left the room {room}")
     
 @sio.on("message")
 def message(data):
@@ -64,12 +94,13 @@ def message(data):
         return
     
     content = {
+        "name" : session.get("name"),
         "message": data["data"]
     }
 
     send(content, to=room)
     rooms[room]["messages"].append(content)
-    print(f"{data['data']}")
+    print(f"{session.get("name")} said: {data['data']}")
 
 if __name__ == "__main__":
     sio.run(app, port='8000', debug=True)
