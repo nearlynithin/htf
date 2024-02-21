facfa699da994a9c1f0b318df75efb945110cccf
nearlynithin <nearlynithin@gmail.com>
2024-02-21 07:42:45 +0530

    full

--- website/views.py
+++ website/views.py
@@ -8,4 +8,5 @@ views = Blueprint('views',__name__)
 @views.route('/')
 @views.route('/home')
 def home():
-    return render_template('home.html')
+    user_name = current_user.firstname if current_user.is_authenticated else "Guest"
+    return render_template('home.html', user_name=user_name)
