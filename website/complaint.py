62c9038d7c527bc9298fe0cebc20f80043d14649
nearlynithin <nearlynithin@gmail.com>
2024-02-21 05:33:39 +0530

    f

--- website/complaint.py
+++ website/complaint.py
@@ -1,15 +1,15 @@
-from flask import Blueprint,render_template,request,flash,redirect,url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import Citizen, Employee, Complaint
 from . import db
-from flask_login import current_user
+from flask_login import current_user, login_required
 from datetime import datetime
+from builtins import isinstance
 
+complaint = Blueprint('complaint', __name__)
 
-complaint = Blueprint('complaint',__name__)
-
-@complaint.route('/complaint',methods=['GET','POST'])
+@complaint.route('/complaint', methods=['GET', 'POST'])
+@login_required
 def add_complaint():
-
     user_complaints = None
 
     if request.method == 'POST':
@@ -24,9 +24,12 @@ def add_complaint():
         db.session.add(new_complaint)
         db.session.commit()
         flash('Complaint submitted successfully!', 'success')
-        user_complaints = Complaint.query.filter_by(citizen_id=current_user.id).all() 
-    
+
+    if current_user.__class__ == Citizen:
+        user_complaints = Complaint.query.filter_by(citizen_id=current_user.id).all()
+
     return render_template('add_complaint.html', user_complaints=user_complaints)
+
 
 @complaint.route('/accept_complaint/<int:complaint_id>')
 def accept_complaint(complaint_id):
